0f3206a37387fea5001e1c471cb82f17361dd770
Lee Martin <hello@leejohnmartin.co.uk>
2022-11-01 20:12:57 +0000

    Fix: Remove old env prefix

--- system/storage/cassandra.ts
+++ system/storage/cassandra.ts
@@ -1,14 +1,13 @@
 import cassandra from "cassandra-driver";
 
 const CASSANDRA_CLIENT_CONFIG = {
-  contactPoints: process.env.OPENFOODDIARY_CASSANDRA_CONTACT_POINTS
-    ? process.env.OPENFOODDIARY_CASSANDRA_CONTACT_POINTS.split(";")
+  contactPoints: process.env.CASSANDRA_CONTACT_POINTS
+    ? process.env.CASSANDRA_CONTACT_POINTS.split(";")
     : ["localhost:9143"],
-  localDataCenter:
-    process.env.OPENFOODDIARY_CASSANDRA_LOCALDATACENTER ?? "datacenter1",
+  localDataCenter: process.env.CASSANDRA_LOCALDATACENTER ?? "datacenter1",
   credentials: {
-    username: process.env.OPENFOODDIARY_CASSANDRA_USER ?? "cassandra",
-    password: process.env.OPENFOODDIARY_CASSANDRA_PASSWORD ?? "cassandra",
+    username: process.env.CASSANDRA_USER ?? "cassandra",
+    password: process.env.CASSANDRA_PASSWORD ?? "cassandra",
   },
 };
 
