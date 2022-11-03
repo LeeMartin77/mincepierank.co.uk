a90206b85d7c1fb083295298bc33564aa46ccdbf
Lee Martin <hello@leejohnmartin.co.uk>
2022-11-03 18:27:42 +0000

    Dev: Limit Local Cassandra Resources

--- .database/cql_setup_local.sh
+++ .database/cql_setup_local.sh
@@ -3,6 +3,9 @@ podman network exists mincepierank-cass-network || podman network create mincepi
 podman start mincepierank-cass || podman run --name mincepierank-cass \
     --network mincepierank-cass-network \
     -e CASSANDRA_CLUSTER_NAME="MincePieRank Test Cluster" \
+    -e MAX_HEAP_SIZE="512M" \
+    -e HEAP_NEWSIZE="100M" \
+    --memory 1g \
     -p 9143:9042 \
     -d \
     docker.io/cassandra
