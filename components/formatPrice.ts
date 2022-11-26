3e0fd513b4eab573eb789c188ebee084cc436433
Lee Martin <hello@leejohnmartin.co.uk>
2022-11-26 13:07:34 +0000

    Fix: Off by one

--- components/formatPrice.ts
+++ components/formatPrice.ts
@@ -1,6 +1,6 @@
 
 export function formatPrice(pencePrice: number): string {
-    if (pencePrice > 100) {
+    if (pencePrice > 99) {
         return `£${(pencePrice / 100).toFixed(2)}`;
     }
     return `£0.${(pencePrice).toFixed(0)}`;
