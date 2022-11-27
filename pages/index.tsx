25bec9560c59c23adb10aa511e391f526f9404fb
Lee Martin <hello@leejohnmartin.co.uk>
2022-11-27 13:33:57 +0000

    Added: Main Categories to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,37 @@
 import Head from "next/head";
 import { InferGetServerSidePropsType } from "next";
-import { 
+import {
   getAllMakerPies,
   getAllPieRankingSummaries,
-  getMincePieMakers, Maker } from "../system/storage";
+  getMincePieMakers,
+  Maker,
+} from "../system/storage";
 import Link from "next/link";
-import { Button, Card, CardActions, CardMedia, Divider, Grid } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  Divider,
+  Grid,
+} from "@mui/material";
 import { mapPiesAndRankings } from "../components/mapPiesAndRankings";
 import { PieSummaryLink } from "../components/pieList/pieList";
 import { descriptionSummary } from "../components/descriptionSummary";
+import { ppCategory } from "../components/formatCategory";
 
 export const getServerSideProps = async () => {
   const data = (await getMincePieMakers()).unwrapOr([]);
   const pies = (await getAllMakerPies()).unwrapOr([]);
   const rankingSummaries = (await getAllPieRankingSummaries()).unwrapOr([]);
-  const { mappedRankings, mappedPies, rankingOrder, unrankedPies } = mapPiesAndRankings(pies, rankingSummaries)
+  const { mappedRankings, mappedPies, rankingOrder, unrankedPies } =
+    mapPiesAndRankings(pies, rankingSummaries);
   const topPieId = rankingOrder.shift();
   return {
     props: {
       makers: data,
       topPie: topPieId ? mappedPies[topPieId] : undefined,
-      topPieRanking: topPieId ? mappedRankings[topPieId] : undefined
+      topPieRanking: topPieId ? mappedRankings[topPieId] : undefined,
     },
   };
 };
@@ -57,10 +68,21 @@ function BrandCard(maker: Maker) {
 function Home({
   makers,
   topPie,
-  topPieRanking
+  topPieRanking,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const topPieMaker = makers.find(x => x.id === topPie?.makerid);
-  const description = topPie && topPieMaker && topPieRanking ? ` ${descriptionSummary(topPie, topPieRanking, topPieMaker)}` : ``
+  const topPieMaker = makers.find((x) => x.id === topPie?.makerid);
+  const description =
+    topPie && topPieMaker && topPieRanking
+      ? ` ${descriptionSummary(topPie, topPieRanking, topPieMaker)}`
+      : ``;
+  const mainCategories = [
+    "classic",
+    "premium",
+    "alternative",
+    "vegan",
+    "mini",
+    "gluten-free",
+  ];
   return (
     <>
       <Head>
@@ -74,14 +96,26 @@ function Home({
         {topPie && topPieRanking && (
           <>
             <h1>Current Top Pie</h1>
-            <PieSummaryLink
-              isTop
-              pie={topPie}
-              ranking={topPieRanking}
-            />
+            <PieSummaryLink isTop pie={topPie} ranking={topPieRanking} />
           </>
         )}
-        <Divider style={{ marginTop: '1em', marginBottom: '1em' }}/>
+        <Divider style={{ marginTop: "1em", marginBottom: "1em" }} />
+        <h2>Categories</h2>
+        <Grid container spacing={2}>
+          {mainCategories.map((categoryid) => (
+            <Grid key={categoryid} item xs={6} sm={4}>
+              <Button
+                LinkComponent={Link}
+                href={`/categories/${categoryid}`}
+                style={{ width: "100%", textAlign: "center" }}
+              >
+                {ppCategory(categoryid)}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+        <Divider style={{ marginTop: "1em", marginBottom: "1em" }} />
+        <h2>Brands</h2>
         <Grid container spacing={2}>
           {makers.map(BrandCard)}
         </Grid>
