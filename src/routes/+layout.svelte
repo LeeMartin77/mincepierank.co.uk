641c9654851251b859dfafa711e100912776d77d
Lee Martin <hello@leejohnmartin.co.uk>
2023-05-28 23:00:11 +0100

    added: Push update to flux repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="notices">
-  <p>Mince Pie Rank is currently read-only until the next Christmas period</p>
+  <p>Mince Pie Rank is read-only until the next Christmas period</p>
   <p>Things might be a bit sparse right now - getting ready for the new season!</p>
 </div>
 
