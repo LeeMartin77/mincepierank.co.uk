afbe11bf927efa3856c39c36e0fabb012391c33c
Lee Martin <hello@leejohnmartin.co.uk>
2023-05-29 11:36:53 +0100

    Added: Filter functionality

--- src/components/FilterablePieList.svelte
+++ src/components/FilterablePieList.svelte
@@ -6,15 +6,45 @@
   export let pies: MakerPie[];
   export let pieRankings: PieRankingSummary[];
   export let fixedCategory: string | undefined = undefined;
-  $: mpr = mapPiesAndRankings(pies, pieRankings);
+  $: availableCategoryIds = Array.from(
+    new Set(
+      pies.reduce<string[]>((acc, curr) => {
+        return [...acc, ...curr.labels.filter((lb) => lb !== fixedCategory)];
+      }, [])
+    )
+  );
+  $: filteredCategoryIds = [] as string[];
+  $: mpr = mapPiesAndRankings(pies, pieRankings, new Set(filteredCategoryIds));
+  $: orderedFilteredPieIds = [...mpr.rankingOrder, ...Array.from(mpr.unrankedPies)];
+  const toggleFilter = (categoryId: string) => {
+    const index = filteredCategoryIds.indexOf(categoryId);
+    if (index !== -1) {
+      filteredCategoryIds.splice(index, 1);
+      filteredCategoryIds = filteredCategoryIds;
+    } else {
+      filteredCategoryIds = [...filteredCategoryIds, categoryId];
+    }
+  };
 </script>
 
 {#if fixedCategory}
   <h3>{ppCategory(fixedCategory)}</h3>
 {/if}
 
+<div class="filters">
+  {#each availableCategoryIds as categoryId}
+    <button
+      class={filteredCategoryIds.includes(categoryId) ? 'selected' : ''}
+      on:click={() => toggleFilter(categoryId)}>{ppCategory(categoryId)}</button
+    >
+  {/each}
+</div>
+
 <div class="pie-list">
-  {#each [...mpr.rankingOrder, ...Array.from(mpr.unrankedPies)] as pieId}
+  {#if orderedFilteredPieIds.length === 0}
+    <span>No pies visibile with selected filters - try removing a filter</span>
+  {/if}
+  {#each orderedFilteredPieIds as pieId}
     <div class="pie-link-card-container">
       <PieLinkCard pie={mpr.mappedPies[pieId]} pieListRanking={mpr.mappedRankings[pieId]} />
     </div>
@@ -34,5 +64,26 @@
   }
   .pie-link-card-container {
     max-width: 320px;
+  }
+
+  .filters {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 0.5em;
+    margin: 1em;
+  }
+  .filters button {
+    cursor: pointer;
+    border: 1px dotted grey;
+    border-radius: 0.5em;
+    background-color: transparent;
+    padding: 0.5em;
+  }
+  .filters button:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  .filters button.selected {
+    border: 1px solid black;
   }
 </style>
--- src/routes/years/[year]/+page.svelte
+++ src/routes/years/[year]/+page.svelte
@@ -7,5 +7,24 @@
   export let data: PageData;
 </script>
 
+<a href={`/years/${data.year}/all-pies`}>View All Pies</a>
+
 <CategoryLinkList year={data.year} categories={data.categories} />
 <BrandLinkList makers={data.makers} year={data.year} />
+
+<style>
+  a {
+    display: block;
+    border: 1px solid black;
+    font-size: 1em;
+    font-weight: bold;
+    border-radius: 0.5em;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    text-align: center;
+    color: initial;
+  }
+  a:visited {
+    color: initial;
+  }
+</style>
