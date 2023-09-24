28108f1c4ae778af4a894afb4d79bac1b5c0c9ce
Lee Martin <hello@leejohnmartin.co.uk>
2023-09-24 11:41:48 +0100

    fix: linting

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,7 +3,7 @@
   import { page } from '$app/stores';
   import type { PageData } from './$types';
 
-  export let data: PageData;
+  //export let data: PageData;
 </script>
 
 {#if $page.data.session && $page.data.session.user}
