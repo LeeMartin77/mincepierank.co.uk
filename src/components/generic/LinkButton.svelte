044cce0ed7fe6c7a9bb01430acf20ded2e2f55f9
Lee Martin <hello@leejohnmartin.co.uk>
2023-10-22 13:31:19 +0100

    visual: Keep underline

--- src/components/generic/LinkButton.svelte
+++ src/components/generic/LinkButton.svelte
@@ -11,7 +11,6 @@
     box-shadow: 0em 0em 0.3em rgba(0, 0, 0, 0.2);
     border-radius: 1em;
     padding: 1em;
-    text-decoration: none;
     text-align: center;
     font-weight: bold;
     color: #111;
