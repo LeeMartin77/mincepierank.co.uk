cd0f71b00687e097ee5251d1d134f99fc6d7876a
Lee Martin <hello@leejohnmartin.co.uk>
2023-10-25 19:47:45 +0100

    added: notice now dismissable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,73 @@
   import { onMount } from 'svelte';
   import type { LayoutData } from './$types';
   import Menu from '$components/Menu.svelte';
+  import LinkButton from '$components/generic/LinkButton.svelte';
   export let data: LayoutData;
+
+  let noticeVisible = false;
+  let noticestring = `${data.readonly ? 'READONLY' : ''}:${!!data.notice ? data.notice : ''}`;
+
+  let cookieNotice = false;
+
+  const noticekey = 'noticestring';
+
+  const cookiekey = 'cookiekey';
 
   onMount(() => {
     if (data.session?.user?.email) {
       // need to look into some form of event for this
       fetch(`/api/claim-anonymous`, { method: 'POST' });
     }
+
+    noticeVisible = data.readonly || !!data.notice;
+
+    let lsstr = localStorage.getItem(noticekey);
+    if (lsstr && lsstr === noticestring) {
+      noticeVisible = false;
+    }
+
+    cookieNotice = !window.location.href.endsWith('/about/cookies');
+    if (localStorage.getItem(cookiekey) === 'accepted') {
+      cookieNotice = false;
+    }
   });
 </script>
 
-{#if data.readonly || data.notice}
+{#if noticeVisible}
   <div class="notices">
-    {#if data.readonly}
-      <p>Mince Pie Rank is read-only until the next Christmas period</p>
-    {/if}
-    {#if data.notice}
-      <p>{data.notice}</p>
-    {/if}
+    <button
+      on:click={() => {
+        localStorage.setItem(noticekey, noticestring);
+        noticeVisible = false;
+      }}>X</button
+    >
+    <div>
+      {#if data.readonly}
+        <p>Mince Pie Rank is read-only until the next Christmas period</p>
+      {/if}
+      {#if data.notice}
+        <p>{data.notice}</p>
+      {/if}
+    </div>
+  </div>
+{/if}
+{#if cookieNotice}
+  <div class="cookie-notice">
+    <div>
+      <p>We use cookies for authentication and associating mince pie rankings to a user</p>
+      <button
+        on:click={() => {
+          localStorage.setItem(cookiekey, 'accepted');
+          cookieNotice = false;
+        }}>Accept</button
+      >
+      <button
+        on:click={() => {
+          cookieNotice = false;
+        }}>Dismiss</button
+      >
+      <a href="/about/cookies">Read Full Policy</a>
+    </div>
   </div>
 {/if}
 <div class="page-container">
@@ -54,6 +103,71 @@
     text-align: center;
     border: 1px dashed grey;
     border-radius: 1em;
+    position: fixed;
+    width: calc(100vw - 4em);
+    background-color: white;
+    top: 1em;
+    left: 1em;
+    display: flex;
+    padding: 1em;
+    align-items: center;
+    gap: 1em;
+    z-index: 99999;
+  }
+
+  .notices button {
+    height: 2em;
+    width: 2em;
+    background: none;
+    border: 1px solid black;
+    border-radius: 0.5em;
+  }
+
+  .cookie-notice {
+    text-align: center;
+    border: 1px dashed grey;
+    border-radius: 1em;
+    position: fixed;
+    width: calc(100vw - 4em);
+    background-color: white;
+    top: 1em;
+    left: 1em;
+    display: flex;
+    padding: 1em;
+    align-items: center;
+    gap: 1em;
+    z-index: 9999999999999;
+  }
+
+  .cookie-notice button {
+    display: block;
+    width: 100%;
+    cursor: pointer;
+    border-radius: 1em;
+    box-shadow: 0em 0em 0.3em rgba(0, 0, 0, 0.2);
+    background: white;
+    border: none;
+    padding: 1em;
+    text-align: center;
+    font-weight: bold;
+    color: #111;
+    margin-bottom: 1em;
+  }
+
+  .cookie-notice a {
+    display: block;
+    border-radius: 1em;
+    padding: 0.5em;
+    text-align: center;
+    font-weight: bold;
+    color: #111;
+  }
+  .cookie-notice a:visited {
+    color: #111;
+  }
+
+  .notices button:hover {
+    cursor: pointer;
   }
 
   .content {
