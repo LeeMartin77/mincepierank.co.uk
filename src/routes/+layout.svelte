41d709eef843514a68f7b341bfd95c37533b4482
Lee Martin <hello@leejohnmartin.co.uk>
2023-10-29 22:19:46 +0000

    added: snow effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,9 +31,105 @@
     if (localStorage.getItem(cookiekey) === 'accepted') {
       cookieNotice = false;
     }
+
+    // snow is a modified version of https://stackoverflow.com/a/13983965
+    (function () {
+      var requestAnimationFrame =
+        window.requestAnimationFrame ||
+        function (callback: any) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+      window.requestAnimationFrame = requestAnimationFrame;
+    })();
+
+    var flakes: any[] = [],
+      canvas = document.getElementById('canvas'),
+      // @ts-ignore
+      ctx = canvas.getContext('2d'),
+      flakeCount = 50,
+      mX = -100,
+      mY = -100;
+    // @ts-ignore
+
+    canvas.width = window.innerWidth;
+    // @ts-ignore
+    canvas.height = window.innerHeight;
+
+    function snow() {
+      // @ts-ignore
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      for (var i = 0; i < flakeCount; i++) {
+        var flake = flakes[i];
+
+        ctx.fillStyle = 'rgba(255,255,255,' + flake.opacity + ')';
+        flake.y += flake.velY;
+        flake.x += flake.velX;
+        // @ts-ignore
+
+        if (flake.y >= canvas.height || flake.y <= 0) {
+          reset(flake);
+        }
+
+        // @ts-ignore
+
+        if (flake.x >= canvas.width || flake.x <= 0) {
+          reset(flake);
+        }
+
+        ctx.beginPath();
+        ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+        ctx.fill();
+      }
+      requestAnimationFrame(snow);
+    }
+
+    function reset(flake: any) {
+      // @ts-ignore
+      flake.x = Math.floor(Math.random() * canvas.width);
+      flake.y = 0;
+      flake.size = Math.random() * 3 + 2;
+      flake.speed = Math.random() * 1 + 0.5;
+      flake.velY = flake.speed;
+      flake.velX = 0;
+      flake.opacity = Math.random() * 0.5 + 0.3;
+    }
+
+    function init() {
+      for (var i = 0; i < flakeCount; i++) {
+        // @ts-ignore
+
+        var x = Math.floor(Math.random() * canvas.width),
+          // @ts-ignore
+
+          y = Math.floor(Math.random() * canvas.height),
+          size = Math.random() * 3 + 2,
+          speed = Math.random() * 1 + 0.5,
+          opacity = Math.random() * 0.5 + 0.3;
+
+        flakes.push({
+          speed: speed,
+          velY: speed,
+          velX: 0,
+          x: x,
+          y: y,
+          size: size,
+          stepSize: Math.random() / 30,
+          step: 0,
+          angle: 180,
+          opacity: opacity
+        });
+      }
+
+      snow();
+    }
+
+    init();
   });
 </script>
 
+<canvas id="canvas" />
 {#if noticeVisible}
   <div class="notices">
     <button
@@ -192,5 +288,19 @@
       min-width: 840px;
       max-width: 1020px;
     }
+  }
+
+  canvas {
+    position: fixed;
+    z-index: -9999999;
+    background-image: linear-gradient(bottom, #c0e1ff 0%, #8ec8ff 84%);
+
+    background-image: -webkit-gradient(
+      linear,
+      left bottom,
+      left top,
+      color-stop(0, #c0e1ff),
+      color-stop(0.84, #8ec8ff)
+    );
   }
 </style>
