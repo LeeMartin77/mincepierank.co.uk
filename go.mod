1b11bfc51ba10ad0b598324dbd5c42db0a32e0cb
Lee <hello@leejohnmartin.co.uk>
2024-04-08 20:50:05 +0100

    feat: postgres setup

--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -18,6 +21,7 @@ require (
 	github.com/oapi-codegen/runtime v1.1.1 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/rs/zerolog v1.32.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/mod v0.16.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/website/api.go
+++ internal/website/api.go
@@ -3,7 +3,11 @@ package website
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
 
 	generated "github.com/leemartin77/mincepierank.co.uk/internal/website/generated"
 )
@@ -15,9 +19,23 @@ var (
 	ErrShutdown error = fmt.Errorf("error shutting down server")
 )
 
-func (wrpr *WebsiteWrapper) HomePage(_ context.Context, req generated.HomePageRequestObject) (generated.HomePageResponseObject, error) {
+func (wrpr *WebsiteWrapper) HomePage(c context.Context, req generated.HomePageRequestObject) (generated.HomePageResponseObject, error) {
+	// urlExample := "postgres://username:password@localhost:5432/mincepierank"
+	conn, err := pgx.Connect(c, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to connect to database")
+		os.Exit(1)
+	}
+	defer conn.Close(c)
 
-	str := "<h1>Hello, World!</h1>"
+	var data string
+	err = conn.QueryRow(c, "select test_data from test_table where id=$1", 1).Scan(&data)
+	if err != nil {
+		log.Error().Err(err).Msg("QueryRow failed")
+		os.Exit(1)
+	}
+
+	str := fmt.Sprintf("<h1>Hello, World!</h1> <h3>%s</h3>", data)
 	reader := strings.NewReader(str)
 
 	return generated.HomePage200TexthtmlResponse{
