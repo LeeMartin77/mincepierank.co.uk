32741a18ba12e87c09bb44a74374f251b9dddaa5
Lee <hello@leejohnmartin.co.uk>
2024-04-16 21:03:36 +0100

    feat: generate other base storage functions

--- internal/storage/types/generate.go
+++ internal/storage/types/generate.go
@@ -1,3 +1,9 @@
 package types
 
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.Maker maker
 //go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.MakerPieYearly maker_pie_yearly
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.MakerPieRankingYearly maker_pie_ranking_yearly
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.UserPieYearly user_pie_yearly
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.UserPieRankingYearly user_pie_ranking_yearly
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.Config config
+//go:generate go run ../../../cmd/storage-generator/main.go github.com/leemartin77/mincepierank.co.uk/internal/storage/types.Admins admins
